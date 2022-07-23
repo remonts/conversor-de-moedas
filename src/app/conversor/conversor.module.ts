f1f648c766a945ab71087c70c931169ee02be1fa
Renan Monteiro <monteiro-renan@hotmail.com>
2022-07-23 15:24:16 -0300

    Criado models do projeto.

---
+++ src/app/conversor/conversor.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConversorComponent } from './conversor/conversor.component';
+
+
+
+@NgModule({
+  declarations: [
+    ConversorComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConversorModule { }
